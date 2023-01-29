2a8aa665f15f6ae935026550273e56aeaa812961
Karim.basher <kareem.basher@gmail.com>
2023-01-29 19:00:13 +0200

    Used body-parser.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
-/* eslint-disable no-console */
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import morgan from 'morgan'
-import * as dotenv from 'dotenv'
+import config from './config'
+import product_routes from './handlers/product'
+import bp from 'body-parser'
 
-dotenv.config()
-
-const PORT = process.env.PORT || 3000
+const PORT = config.port
 // create an instance server
 const app: Application = express()
 // HTTP request logger middleware
 app.use(morgan('short'))
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
-// add routing for / path
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello World 🌍'
-  })
-})
-
-// start express server
 app.listen(PORT, () => {
-  console.log(`Server is starting at prot:${PORT}`)
+  console.log(`Server is starting at port:${PORT}`)
 })
+
+product_routes(app)
 
 export default app
