0414528ad8d321753177dfff98cddfc34400234f
kareembasher <kareem.basher@gmail.com>
2023-02-03 20:16:25 +0200

    Added gender, order history, and favorites to user types.

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,13 +1,27 @@
 import { ObjectId } from 'mongodb'
+import OrderHistory from './order.type'
+
+interface Alarm {
+  name: string
+  notes: string | null
+  dose: number
+  time: string[] | null
+  repetition: string | null
+}
 
 type User = {
   id?: ObjectId
   name: string
   email: string
   password: string
+  gender?: string
   address?: string | null
   age?: number | null
   phoneNumber?: string | null
+  alarms?: Alarm[] | null
+  searchHistory?: string[] | null
+  favorites?: ObjectId[] | null
+  orderHistory?: OrderHistory[] | null
 }
 
 export default User
