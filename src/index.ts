8704c3f06a4406bb0edd1bd74eb6bf64b17f5374
Karim.basher <kareem.basher@gmail.com>
2023-02-13 19:15:20 +0200

    Changed accepted origin to all.

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app: Application = express()
 app.use(morgan('short'))
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
-app.use(cors({ origin: 'http://127.0.0.1:5173' }))
+app.use(cors({ origin: '*' }))
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`)
