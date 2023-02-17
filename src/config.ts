08aec4f1610c92e81a13a7e6d33a75e7a8ba6d31
Karim.basher <kareem.basher@gmail.com>
2023-02-17 23:45:32 +0200

    Added sendgridEmail env variable to config.

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const { PORT, BCRYPT_SECRET, SALT, SIGNITURE, MONGODB_STRING, SENDGRID_API } = process.env
+const { PORT, BCRYPT_SECRET, SALT, SIGNITURE, MONGODB_STRING, SENDGRID_API, SENDGRID_EMAIL } =
+  process.env
 
 export default {
   port: PORT,
@@ -10,5 +11,6 @@ export default {
   salt: SALT,
   signiture: SIGNITURE,
   dbString: MONGODB_STRING,
-  sendgridKey: SENDGRID_API
+  sendgridKey: SENDGRID_API,
+  sendgridEmail: SENDGRID_EMAIL
 }
