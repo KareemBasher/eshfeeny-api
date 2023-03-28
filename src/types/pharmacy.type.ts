9a263d15e84a43909984602403ab54a9af26e663
Karim.basher <kareem.basher@gmail.com>
2023-03-28 16:15:08 +0200

    Created Pharmacy types.

---
+++ src/types/pharmacy.type.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from 'mongodb'
+
+interface Orders {
+  productId: ObjectId
+  manufacturerId: string
+  quantity: number
+}
+
+type Pharmacy = {
+  _id?: ObjectId
+  name: string
+  email: string
+  password: string
+  phoneNumber: string
+  orders: Orders[]
+}
+
+export default Pharmacy
