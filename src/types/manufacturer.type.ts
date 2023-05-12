3c5624dc399ad43265f72e2aacd584876c49ed78
Karim.basher <kareem.basher@gmail.com>
2023-05-12 17:42:21 +0200

    Created manufacturer type.

---
+++ src/types/manufacturer.type.ts
@@ -0,0 +1,20 @@
+import { ObjectId } from 'mongodb'
+
+interface Order {
+  _id?: ObjectId
+  pharmacyId: ObjectId
+  product: ObjectId
+  quantity: number
+  createdOn: Date
+}
+
+type Manufacturer = {
+  _id?: ObjectId
+  name: string
+  email: string
+  password: string
+  orders: Order[]
+  delayedOrders: Order[]
+}
+
+export default Manufacturer
